ebcba571bd00eb8b0cf72df782968f364bded054
Minhaz Kamal <70626937+minhazkamal@users.noreply.github.com>
2022-05-06 19:02:08 +0600

    Initial Commit

---
+++ image_class.js
@@ -0,0 +1,56 @@
+const ARB_NUMBER_CLASSES = {
+    0: 'Sifr',
+    1: 'WaaHid',
+    2: 'Ithnaan',
+    3: 'Thalaatha',
+    4: 'Arbiya',
+    5: 'Khamsa',
+    6: 'Sitta',
+    7: 'Sabya',
+    8: 'Thamaaniya',
+    9: 'Tisya'
+}
+
+const ENG_NUMBER_CLASSES = {
+    0: 'Zero',
+    1: 'One',
+    2: 'Two',
+    3: 'Three',
+    4: 'Four',
+    5: 'Five',
+    6: 'Six',
+    7: 'Seven',
+    8: 'Eight',
+    9: 'Nine'
+}
+
+// const IMAGE_CLASSES = {
+//     0: 'alif',
+//     1: 'Ba',
+//     2: 'Ta',
+//     3: 'Tha',
+//     4: 'Jim',
+//     5: 'Haa',
+//     6: 'Kha',
+//     7: 'Dal',
+//     8: 'Dhal',
+//     9: 'Ra',
+//     10: 'Zay',
+//     11: 'Sin',
+//     12: 'Shin',
+//     13: 'Soad',
+//     14: 'Doad',
+//     15: 'Toa',
+//     16: 'Zoa',
+//     17: 'ayn',
+//     18: 'Ghayn',
+//     19: 'Fa',
+//     20: 'Qaf',
+//     21: 'Kaf',
+//     22: 'Lam',
+//     23: 'Mim',
+//     24: 'Nun',
+//     25: 'Ha',
+//     26: 'Waw',
+//     27: 'Ya'
+// }
